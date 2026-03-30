2b5b567f73280b88d59eff6142dd7289b4b4a9d6
Alexander Sprague <a.lee.sprague@gmail.com>
2026-03-30 10:51:44 -0500

    Working on remove dinosaur page, trying to make each dino card a functional button

--- Ark-Breeding-Calculator/Ark-Breeding-Calculator/obj/Debug/net10.0/Ark-Breeding-Calculator.AssemblyInfo.cs
+++ Ark-Breeding-Calculator/Ark-Breeding-Calculator/obj/Debug/net10.0/Ark-Breeding-Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ark-Breeding-Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9517fb62b9badc2c8f5e578b22c8086ab17d45c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d297e2fe99d032c1655a3324a252c5c25d8eb120")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ark-Breeding-Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ark-Breeding-Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
